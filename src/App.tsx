7a0942b5b07357a0193955cdb4112151fe78847e
EmmanuelOkite <your-github-emmanuelokite3@gmail.com>
2026-07-18 20:00:17 +0300

    added the lohin import on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
 import AddStudent from './student/AddStudent';
 import Register from './auth/Register';
+import Login from './auth/Login';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/students/add" element={<AddStudent />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
